fee9b05bad8b8b0ea40a51846b001f37f71c58ff
algosay <algosay26@gmail.com>
2026-07-31 11:14:13 +0530

    MISSION VISION ADD-5

--- src/HomeView.js
+++ src/HomeView.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Sparkles } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { TypeAnimation } from 'react-type-animation';
 import AlgoSayLogo from './AlgoSayLogo'; 
